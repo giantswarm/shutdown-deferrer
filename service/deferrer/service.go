27b2cd7101dde70cdf411240434577395d074089
Tuomas Mäkinen <1947505+tuommaki@users.noreply.github.com>
2018-10-18 13:49:14 +0300

    Implement deferrer.Service.ShouldDrain() (#7)
    Add implementation for ShouldDrain() that finds out if node termination
    in current POD should be deferred. This is decided by presense and
    status conditions in corresponding DrainerConfig.

--- service/deferrer/service.go
+++ service/deferrer/service.go
@@ -1,9 +1,21 @@
 package deferrer
 
 import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/giantswarm/apiextensions/pkg/apis/core/v1alpha1"
 	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metasv1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	EnvKeyMyPodName      = "MY_POD_NAME"
+	EnvKeyMyPodNamespace = "MY_POD_NAMESPACE"
 )
 
 type Config struct {
@@ -30,4 +42,91 @@ func New(config Config) (*Service, error) {
 	}
 
 	return s, nil
+}
+
+// ShouldDefer finds corresponding DrainerConfig for the POD it's running in
+// and checks if node in current POD is drained yet. If DrainerConfig doesn't
+// exist or doesn't have Drained or Timeout condition, node termination should
+// be deferred.
+//
+// Current POD name and namespace are picked from environment variables with
+// corresponding keys defined in constants EnvKeyMyPodName &
+// EnvKeyMyPodNamespace. Defining these env variables is most conveniently
+// achieved by utilizing Kubernetes Downward API:
+// https://kubernetes.io/docs/tasks/inject-data-application/environment-variable-expose-pod-information/
+func (s *Service) ShouldDefer(ctx context.Context) (bool, error) {
+	var err error
+
+	var podName, podNamespace string
+	{
+		s.logger.LogCtx(ctx, "level", "debug", "message", "finding pod name and namespace")
+
+		podName, err = s.getPodName()
+		if err != nil {
+			return false, microerror.Mask(err)
+		}
+		podNamespace, err = s.getPodNamespace()
+		if err != nil {
+			return false, microerror.Mask(err)
+		}
+
+		s.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("found pod name and namespace: %s, %s", podName, podNamespace))
+	}
+
+	var drainerConfig *v1alpha1.DrainerConfig
+	{
+		s.logger.LogCtx(ctx, "level", "debug", "message", "finding drainerconfig for pod")
+
+		drainerConfig, err = s.g8sClient.CoreV1alpha1().DrainerConfigs(podNamespace).Get(podName, metasv1.GetOptions{})
+		if apierrors.IsNotFound(err) {
+			s.logger.LogCtx(ctx, "level", "debug", "message", "did not find drainerconfig")
+			drainerConfig = nil
+		} else if err != nil {
+			return true, microerror.Mask(err)
+		}
+
+		s.logger.LogCtx(ctx, "level", "debug", "message", "found drainerconfig for pod")
+	}
+
+	{
+		s.logger.LogCtx(ctx, "level", "debug", "message", "finding if node termination has to be defered")
+
+		if drainerConfig == nil {
+			s.logger.LogCtx(ctx, "level", "debug", "message", "found node termination should be deferred")
+			s.logger.LogCtx(ctx, "level", "debug", "message", "pod drainerconfig does not exist")
+			return true, nil
+		}
+
+		if drainerConfig.Status.HasDrainedCondition() {
+			s.logger.LogCtx(ctx, "level", "debug", "message", "found node termination does not have to be deferred")
+			s.logger.LogCtx(ctx, "level", "debug", "message", "pod drainerconfig has drained condition")
+			return false, nil
+		}
+		if drainerConfig.Status.HasTimeoutCondition() {
+			s.logger.LogCtx(ctx, "level", "debug", "message", "found node termination does not have to be deferred")
+			s.logger.LogCtx(ctx, "level", "debug", "message", "pod drainerconfig has timeout condition")
+			return false, nil
+		}
+
+		s.logger.LogCtx(ctx, "level", "debug", "message", "found node termination should be deferred")
+		return true, nil
+	}
+}
+
+func (s *Service) getPodName() (string, error) {
+	podName := os.Getenv(EnvKeyMyPodName)
+	if podName == "" {
+		return "", microerror.Maskf(invalidConfigError, "pod name not present in runtime - make sure $%s is set", EnvKeyMyPodName)
+	}
+
+	return podName, nil
+}
+
+func (s *Service) getPodNamespace() (string, error) {
+	podNamespace := os.Getenv(EnvKeyMyPodNamespace)
+	if podNamespace == "" {
+		return "", microerror.Maskf(invalidConfigError, "pod namespace not present in runtime - make sure $%s is set", EnvKeyMyPodNamespace)
+	}
+
+	return podNamespace, nil
 }
