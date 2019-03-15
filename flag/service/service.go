4e7d2b73859ea7dac1a2138e04e07fa5870d109b
Ross Fairbanks <rossf7@users.noreply.github.com>
2019-03-15 09:48:03 +0100

    Add kubeconfig support (#11)
    * vendor: updated operatorkit
    
    * Flag changes

--- flag/service/service.go
+++ flag/service/service.go
@@ -1,6 +1,6 @@
 package service
 
-import "github.com/giantswarm/shutdown-deferrer/flag/service/kubernetes"
+import "github.com/giantswarm/operatorkit/flag/service/kubernetes"
 
 // Service is an intermediate data structure for command line configuration flags.
 type Service struct {
